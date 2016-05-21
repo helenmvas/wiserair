edf0a322a2c7b1072f9a02b3e42347589a34c845
Helen Vasconcelos <helenmvas@gmail.com>
2016-05-21 15:00:40 -0300

    adding chart container div

--- wp-content/themes/_tk-master/page-up-close.php
+++ wp-content/themes/_tk-master/page-up-close.php
@@ -48,6 +48,10 @@ get_header(); ?>
 			<div class="col-lg-12 text-center">
 				<?php echo do_shortcode('[wonderplugin_tabs id="3"]'); ?>
 			</div>
+
+			<div id="chart-container">
+
+			</div>
 			<script src="https://code.highcharts.com/highcharts.js"></script>
 			<script src="https://code.highcharts.com/modules/exporting.js"></script>
 			<script type="text/javascript">
