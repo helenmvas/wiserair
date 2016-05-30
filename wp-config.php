b772e0bc7f2fbce763778161d6bb3a6c27f3fa18
Helen Vasconcelos <helenmvas@gmail.com>
2016-05-30 13:15:39 -0300

    multisite setup

--- wp-config.php
+++ wp-config.php
@@ -81,6 +81,14 @@ define('WP_DEBUG', false);
 
 
 define( 'WP_ALLOW_MULTISITE', true );
+
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'wiserairmlp-dev.azurewebsites.net');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
