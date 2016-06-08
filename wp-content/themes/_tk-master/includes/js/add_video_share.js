71a94cda26b9d850de23268cc520e6408ab0a32d
Helen Vasconcelos <helenmvas@gmail.com>
2016-06-08 16:11:41 -0300

    onclick test

---
+++ wp-content/themes/_tk-master/includes/js/add_video_share.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(
+
+  function($) {
+    
+ 
+    $('.wplightbox').click(
+      function() {
+        //$('#html5-text').append('<div class="share_link">Share</div>');
+        alert('a');
+      }
+    );
+
+
+  }
+
+
+
+);
