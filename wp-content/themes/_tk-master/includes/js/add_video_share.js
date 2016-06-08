8a2a6169371ea8e8248fa846c890393a6341187d
Helen Vasconcelos <helenmvas@gmail.com>
2016-06-08 16:18:39 -0300

    test

--- wp-content/themes/_tk-master/includes/js/add_video_share.js
+++ wp-content/themes/_tk-master/includes/js/add_video_share.js
@@ -1,25 +1,33 @@
-jQuery(document).ready(
+// jQuery(document).ready(
 
-  function($) {
+//   function($) {
     
  
-    $('.wplightbox').click(
-      function() {
-        //$('#html5-text').append('<div class="share_link">Share</div>');
-        alert('a');
-      }
-    );
+//     $('.wplightbox').click(
+//       function() {
+//         //$('#html5-text').append('<div class="share_link">Share</div>');
+//         alert('a');
+//       }
+//     );
 
-    $('#html5-text').load(
-      function() {
-        alert('b');
-      }
+//     $('#html5-text').load(
+//       function() {
+//         alert('b');
+//       }
 
-    );
+//     );
 
 
+//   }
+
+
+
+// );
+
+jQuery('#html5-elem-box').load (
+
+  function($) {
+    alert('a');
   }
-
-
 
 );
