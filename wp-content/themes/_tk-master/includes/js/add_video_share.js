191048f0c56aaa7c7beb99ee37aeb08600cdf0d7
Helen Vasconcelos <helenmvas@gmail.com>
2016-06-09 16:42:08 -0300

    grabbing iframe link

--- wp-content/themes/_tk-master/includes/js/add_video_share.js
+++ wp-content/themes/_tk-master/includes/js/add_video_share.js
@@ -1,37 +1,7 @@
-jQuery(document).ready(
-
-  function($) {
-    
- 
-    // $('.wplightbox').click(
-    //   function() {
-    //     //$('#html5-text').append('<div class="share_link">Share</div>');
-    //     alert('a');
-    //   }
-    // );
-
-    // $('#html5-text').load(
-    //   function() {
-    //     alert('b');
-    //   }
-
-    // );
-
-    // function createMarkeup() {
-    //   alert('a');
-    // }
-
-
-  }
-
-
-
-);
-
-
 jQuery(document).on('DOMNodeInserted', function(e) {
     if (e.target.id == 'html5-text') {
-      alert('a');
-       jQuery(e).append('<div class="videoShare">APPEND</div>');
+      var link = jQuery('#html5boxiframevideo').attr('src');
+      alert("link = " + link);
+      jQuery('#html5-text').append('<a href="'+link+'" class="videoShare">SHARE</a>');
     }
 });
