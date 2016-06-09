cac4991a2f1155dd4f3b295c6f1bbd58b870fa03
Helen Vasconcelos <helenmvas@gmail.com>
2016-06-09 16:47:48 -0300

    fixing src selection

--- wp-content/themes/_tk-master/includes/js/add_video_share.js
+++ wp-content/themes/_tk-master/includes/js/add_video_share.js
@@ -1,7 +1,9 @@
 jQuery(document).on('DOMNodeInserted', function(e) {
     if (e.target.id == 'html5-text') {
-      var link = jQuery('#html5boxiframevideo').attr('src');
+      var link = jQuery('iframe#html5boxiframevideo').attr('src');
       alert("link = " + link);
-      jQuery('#html5-text').append('<a href="'+link+'" class="videoShare">SHARE</a>');
+      if(link) {
+        jQuery('#html5-text').append('<a href="'+link+'" class="videoShare">SHARE</a>');
+      }
     }
 });
