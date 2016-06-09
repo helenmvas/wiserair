170e62ba7a13ca97efdd5880850c96ecfc60f332
Helen Vasconcelos <helenmvas@gmail.com>
2016-06-09 15:58:38 -0300

    trying to add create markeup fucntion

--- wp-content/themes/_tk-master/includes/js/add_video_share.js
+++ wp-content/themes/_tk-master/includes/js/add_video_share.js
@@ -1,33 +1,29 @@
-// jQuery(document).ready(
-
-//   function($) {
-    
- 
-//     $('.wplightbox').click(
-//       function() {
-//         //$('#html5-text').append('<div class="share_link">Share</div>');
-//         alert('a');
-//       }
-//     );
-
-//     $('#html5-text').load(
-//       function() {
-//         alert('b');
-//       }
-
-//     );
-
-
-//   }
-
-
-
-// );
-
-jQuery('#html5-text').ready (
+jQuery(document).ready(
 
   function($) {
-    alert('a');
+    
+ 
+    // $('.wplightbox').click(
+    //   function() {
+    //     //$('#html5-text').append('<div class="share_link">Share</div>');
+    //     alert('a');
+    //   }
+    // );
+
+    // $('#html5-text').load(
+    //   function() {
+    //     alert('b');
+    //   }
+
+    // );
+
+    function createMarkeup() {
+      alert('a');
+    }
+
+
   }
+
+
 
 );
