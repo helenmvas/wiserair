c51d48859e60ea01245328a4fa708ed4298dddaf
Helen Vasconcelos <helenmvas@gmail.com>
2016-06-10 14:25:36 -0300

    calling php function

--- wp-content/themes/_tk-master/includes/js/add_video_share.js
+++ wp-content/themes/_tk-master/includes/js/add_video_share.js
@@ -2,8 +2,10 @@ jQuery(document).on('DOMNodeInserted', function(e) {
     if (e.target.id == 'html5-text') {
       var link = jQuery('iframe#html5boxiframevideo').attr('src');
       alert("link = " + link);
-      if(link) {
-        jQuery('#html5-text').append('<a href="'+link+'" class="videoShare">SHARE</a>');
-      }
+      // if(link) {
+        // jQuery('#html5-text').append('<a href="'+link+'" class="videoShare">SHARE</a>');
+      // }
+
+      jQuery('#html5-text').load('./socialShareButtons.php');
     }
 });
---
+++ wp-content/themes/_tk-master/includes/js/socialShareButtons.php
@@ -0,0 +1,5 @@
+<?php
+
+echo do_shortcode('[apss_share]');
+
+?>
