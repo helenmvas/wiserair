6f90e945f636d3979b50ac11162332312667867c
Helen Vasconcelos <helenmvas@gmail.com>
2016-06-10 15:45:31 -0300

    commenting out code

--- wp-content/themes/_tk-master/includes/js/add_video_share.js
+++ wp-content/themes/_tk-master/includes/js/add_video_share.js
@@ -10,34 +10,34 @@ function getFacebookShareHTML( link ) {
   return '<a rel="nofollow" onclick="apss_open_in_popup_window(\''+facebookShareLink+'\');" title="Share on Facebook" target href="'+facebookShareLink+'" title="Share on Facebook"><div class="apss-icon-block clearfix"><i class="fa fa-facebook"></i></div></a>';
 }
 
-function getTwitterShareURL() {
+function getTwitterShareURL( link ) {
 
 }
 
-function getGooglePlusShareURL() {
+function getGooglePlusShareURL( link ) {
 
 }
 
-function getLinkedInShareURL() {
+function getLinkedInShareURL( link ) {
 
 
 }
 
-function getEmailShareURL() {
+function getEmailShareURL( link ) {
 
 }
 
 jQuery(document).on('DOMNodeInserted', function(e) {
     if (e.target.id == 'html5-text') {
       var link = jQuery('iframe#html5boxiframevideo').attr('src');
-      alert("link = " + link);
+      // alert("link = " + link);
       // if(link) {
         // jQuery('#html5-text').append('<a href="'+link+'" class="videoShare">SHARE</a>');
       // }
 
       link = 'https://www.youtube.com/embed/Z6Cw-ucxkJk';
       
-      jQuery('#html5-text').append( getFacebookShareHTML() + getTwitterShareURL() + getGooglePlusShareURL() + getLinkedInShareURL() + getEmailShareURL() );
+      jQuery('#html5-text').append( getFacebookShareHTML(link) + getTwitterShareURL() + getGooglePlusShareURL() + getLinkedInShareURL() + getEmailShareURL() );
     }
 });
 
