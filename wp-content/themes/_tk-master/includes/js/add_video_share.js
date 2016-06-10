2c0a5396c9c4639a4f305d69602893b1a7f047aa
Helen Vasconcelos <helenmvas@gmail.com>
2016-06-10 16:29:37 -0300

    fixing typo

--- wp-content/themes/_tk-master/includes/js/add_video_share.js
+++ wp-content/themes/_tk-master/includes/js/add_video_share.js
@@ -17,7 +17,7 @@ function getTwitterShareURL( link ) {
 
 function getGooglePlusShareURL( link ) {
   var googleShareLink = 'https://plus.google.com/share?url=' + link;
-  return '<a rel="nofollow" onclick="apss_open_in_popup_window(\''+googleShareLink+'\');" title="Share on Google Plus" target="" href="'+googleShareLink+'"><div class="apss-icon-block clearfix"><i class="fa fa-google-plus"></i><span class="apss-social-text">Share on Google Plus</span><span class="apss-share">Share</span></div></a>';
+  return '<a rel="nofollow" onclick="apss_open_in_popup_window(\''+googleShareLink+'\');" title="Share on Google Plus" target="" href="'+googleShareLink+'"><div class="apss-icon-block clearfix"><i class="fa fa-google-plus"></i></div></a>';
 }
 
 // function getLinkedInShareURL( link ) {
@@ -39,7 +39,7 @@ jQuery(document).on('DOMNodeInserted', function(e) {
 
       link = 'https://www.youtube.com/embed/Z6Cw-ucxkJk';
       
-      jQuery('#html5-text').append( getFacebookShareHTML(link)  + getTwitterShareURL(link) + getGooglePlusShareURL() /**+ getLinkedInShareURL() + getEmailShareURL() **/ );
+      jQuery('#html5-text').append( getFacebookShareHTML(link)  + getTwitterShareURL(link) + getGooglePlusShareURL(link) /**+ getLinkedInShareURL() + getEmailShareURL() **/ );
     }
 });
 
