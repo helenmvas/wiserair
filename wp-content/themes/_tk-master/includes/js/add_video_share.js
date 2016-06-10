d6db3d19e7f65ca4dfc72eff7d97e1c5f77aa228
Helen Vasconcelos <helenmvas@gmail.com>
2016-06-10 15:42:03 -0300

    cleaning up code

--- wp-content/themes/_tk-master/includes/js/add_video_share.js
+++ wp-content/themes/_tk-master/includes/js/add_video_share.js
@@ -5,6 +5,28 @@ function apss_open_in_popup_window(url){
     window.open(url, 'fdadas', 'toolbars=0,width=640,height=320,left=200,top=200,scrollbars=1,resizable=1');
 }
 
+function getFacebookShareHTML( link ) {
+  facebookShareLink = 'https://www.facebook.com/sharer/sharer.php?u='+link;
+  return '<a rel="nofollow" onclick="apss_open_in_popup_window(\''+facebookShareLink+'\');" title="Share on Facebook" target href="'+facebookShareLink+'" title="Share on Facebook"><div class="apss-icon-block clearfix"><i class="fa fa-facebook"></i></div></a>';
+}
+
+function getTwitterShareURL() {
+
+}
+
+function getGooglePlusShareURL() {
+
+}
+
+function getLinkedInShareURL() {
+
+
+}
+
+function getEmailShareURL() {
+
+}
+
 jQuery(document).on('DOMNodeInserted', function(e) {
     if (e.target.id == 'html5-text') {
       var link = jQuery('iframe#html5boxiframevideo').attr('src');
@@ -14,8 +36,8 @@ jQuery(document).on('DOMNodeInserted', function(e) {
       // }
 
       link = 'https://www.youtube.com/embed/Z6Cw-ucxkJk';
-      facebookShareLink = 'https://www.facebook.com/sharer/sharer.php?u='+link;
-      jQuery('#html5-text').append('<a rel="nofollow" onclick="apss_open_in_popup_window(\''+facebookShareLink+'\');" title="Share on Facebook" target href="'+facebookShareLink+'" title="Share on Facebook"><div class="apss-icon-block clearfix"><i class="fa fa-facebook"></i></div></a>');
+      
+      jQuery('#html5-text').append( getFacebookShareHTML() + getTwitterShareURL() + getGooglePlusShareURL() + getLinkedInShareURL() + getEmailShareURL() );
     }
 });
 
