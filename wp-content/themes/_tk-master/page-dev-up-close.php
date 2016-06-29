9aad6bd8aca243d556ed9b8e6cf30cf013c91202
Brittany Glover <brittg88@me.com>
2016-06-29 13:18:11 -0400

    up close format

--- wp-content/themes/_tk-master/page-dev-up-close.php
+++ wp-content/themes/_tk-master/page-dev-up-close.php
@@ -17,54 +17,7 @@ get_header(); ?>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script src="<? echo get_template_directory_uri() ?>/includes/js/upclose_highchart.js"></script>
 
-
-
-<section class="text-center blue">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-6 col-md-6 col-sm-6" id="eco-iq">
-				<img src="<?php the_field('eco_iq_image'); ?>"/>
-				<h2><?php the_field('eco_iq_title'); ?></h2>
-				<p class="subtitle"><?php the_field('eco_iq_text'); ?></p>
-			</div>
-			<div class="col-lg-6 col-md-6 col-sm-6" id="wiser-forecast">
-				<img src="<?php the_field('wiser_forecast_image'); ?>"/>
-				<h2><?php the_field('wiser_forecast_title'); ?></h2>
-				<p class="subtitle"><?php the_field('wiser_forecast_text'); ?></p>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section class="text-center">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-6 col-md-6 col-sm-6 text-center" id="comfort-boost" style="background-image: url('<?php the_field('comfort_boost_background_image'); ?>')">>
-				<img src="<?php the_field('comfort_boost_image'); ?>"/>
-				<h2><?php the_field('comfort_boost_title'); ?></h2>
-				<p class="subtitle"><?php the_field('comfort_boost_text'); ?></p>
-			</div>
-			<div class="col-lg-6 col-md-6 col-sm-6" id="my-wiser" style="background-image: url('<?php the_field('my_wiser_background_image'); ?>')">
-				<h2><?php the_field('my_wiser_title'); ?></h2>
-				<p class="subtitle"><?php the_field('my_wiser_text'); ?></p>
-			</div>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section class="text-center dark-grey" id="bells">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12 col-md-12 col-sm-12 text-center">
-				<h2>Wiser Pulse</h2>
-				<p>TBC</p>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section class="text-center">
+<section class="text-center light-grey">
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-6 col-md-6 col-sm-6 text-center" id="touchscreen">
@@ -95,6 +48,34 @@ get_header(); ?>
 	</div>
 </section>
 
+<section class="text-center light-grey">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-sm-6 text-center" id="comfort-boost" style="background-image: url('<?php the_field('comfort_boost_background_image'); ?>')">>
+				<img src="<?php the_field('comfort_boost_image'); ?>"/>
+				<h2><?php the_field('comfort_boost_title'); ?></h2>
+				<p class="subtitle"><?php the_field('comfort_boost_text'); ?></p>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6" id="my-wiser" style="background-image: url('<?php the_field('my_wiser_background_image'); ?>')">
+				<h2><?php the_field('my_wiser_title'); ?></h2>
+				<p class="subtitle"><?php the_field('my_wiser_text'); ?></p>
+			</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="text-center dark-grey" id="bells">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 text-center">
+				<h2>Wiser Pulse</h2>
+				<p>TBC</p>
+			</div>
+		</div>
+	</div>
+</section>
+
 <section class="text-center">
 	<div class="container-fluid">
 		<div class="row">
@@ -105,6 +86,23 @@ get_header(); ?>
 			<div class="col-lg-6 col-md-6 col-sm-6 text-center" id="auto-updates" style="background-image: url('<?php the_field('auto_updates_background_image'); ?>')">
 				<h2><?php the_field('auto_updates_title'); ?></h2>
 				<p class="subtitle"><?php the_field('auto_updates_text'); ?></p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="text-center">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-sm-6" id="eco-iq">
+				<img src="<?php the_field('eco_iq_image'); ?>"/>
+				<h2><?php the_field('eco_iq_title'); ?></h2>
+				<p class="subtitle"><?php the_field('eco_iq_text'); ?></p>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6" id="wiser-forecast">
+				<img src="<?php the_field('wiser_forecast_image'); ?>"/>
+				<h2><?php the_field('wiser_forecast_title'); ?></h2>
+				<p class="subtitle"><?php the_field('wiser_forecast_text'); ?></p>
 			</div>
 		</div>
 	</div>
