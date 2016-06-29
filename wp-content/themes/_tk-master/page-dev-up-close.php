e0197f549682dac359f232642c36ab05f9d56306
Brittany Glover <brittg88@me.com>
2016-06-29 12:07:59 -0400

    up close revisions

--- wp-content/themes/_tk-master/page-dev-up-close.php
+++ wp-content/themes/_tk-master/page-dev-up-close.php
@@ -31,27 +31,16 @@ get_header(); ?>
 	</div>
 </section>
 
-<section class="text-center" id="eco-iq">
+<section class="text-center">
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-12 text-center">
-				<div><img src="<?php the_field('eco_iq_image'); ?>"/></div>
-			</div>
-			<div class="col-lg-12 text-center">
+			<div class="col-lg-6 col-md-6 col-sm-6" id="eco-iq">
+				<img src="<?php the_field('eco_iq_image'); ?>"/></div>
 				<h2><?php the_field('eco_iq_title'); ?></h2>
 				<p class="subtitle"><?php the_field('eco_iq_text'); ?></p>
 			</div>
-		</div>
-	</div>
-</section>
-
-<section class="text-center" id="wiser-forecast">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12 text-center">
-				<div><img src="<?php the_field('wiser_forecast_image'); ?>"/></div>
-			</div>
-			<div class="col-lg-12 text-center">
+			<div class="col-lg-6 col-md-6 col-sm-6" id="wiser-forecast">
+				<div><img src="<?php the_field('wiser_forecast_image'); ?>"/>
 				<h2><?php the_field('wiser_forecast_title'); ?></h2>
 				<p class="subtitle"><?php the_field('wiser_forecast_text'); ?></p>
 			</div>
@@ -62,10 +51,8 @@ get_header(); ?>
 <section class="text-center" id="bells">
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-12 text-center">
+			<div class="col-lg-12 col-md-12 col-sm-12 text-center">
 				<h2>Wiser Pulse / Touchscreen / Smart Sense</h2>
-			</div>
-			<div class="col-lg-12 text-center">
 				<?php echo do_shortcode('[wonderplugin_tabs id="9"]'); ?>
 			</div>
 		</div>
@@ -75,15 +62,15 @@ get_header(); ?>
 <section class="text-center up-close-three">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-lg-3 text-center" id="my-wiser" style="background-image: url('<?php the_field('my_wiser_background_image'); ?>')">
+			<div class="col-lg-4 col-md-4 col-sm-4" id="my-wiser" style="background-image: url('<?php the_field('my_wiser_background_image'); ?>')">
 				<h2><?php the_field('my_wiser_title'); ?></h2>
 				<p class="subtitle"><?php the_field('my_wiser_text'); ?></p>
 			</div>
-			<div class="col-lg-3 text-center" id="schedules" style="background-image: url('<?php the_field('schedules_background_image'); ?>')">
+			<div class="col-lg-4 col-md-4 col-sm-4" id="schedules" style="background-image: url('<?php the_field('schedules_background_image'); ?>')">
 				<h2><?php the_field('schedules_title'); ?></h2>
 				<p class="subtitle"><?php the_field('schedules_text'); ?></p>
 			</div>
-			<div class="col-lg-3 text-center" id="ready-mode" style="background-image: url('<?php the_field('ready_mode_background_image'); ?>')">
+			<div class="col-lg-4 col-md-4 col-sm-4" id="ready-mode" style="background-image: url('<?php the_field('ready_mode_background_image'); ?>')">
 				<h2><?php the_field('ready_mode_title'); ?></h2>
 				<p class="subtitle"><?php the_field('ready_mode_text'); ?></p>
 			</div>
@@ -94,11 +81,11 @@ get_header(); ?>
 <section class="text-center up-close-three">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-lg-6 text-center" id="energy-insight" style="background-image: url('<?php the_field('energy_insights_background_image'); ?>')">
+			<div class="col-lg-6 col-md-6 col-sm-6 text-center" id="energy-insight" style="background-image: url('<?php the_field('energy_insights_background_image'); ?>')">
 				<h2><?php the_field('energy_insights_title'); ?></h2>
 				<p class="subtitle"><?php the_field('energy_insights_text'); ?></p>
 			</div>
-			<div class="col-lg-6 text-center" id="auto-updates" style="background-image: url('<?php the_field('auto_updates_background_image'); ?>')">
+			<div class="col-lg-6 col-md-6 col-sm-6 text-center" id="auto-updates" style="background-image: url('<?php the_field('auto_updates_background_image'); ?>')">
 				<h2><?php the_field('auto_updates_title'); ?></h2>
 				<p class="subtitle"><?php the_field('auto_updates_text'); ?></p>
 			</div>
@@ -109,7 +96,7 @@ get_header(); ?>
 <section class="homepage-section-7" id="homepage-slideshow">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-lg-12 text-center">
+			<div class="text-center">
 				<?php echo do_shortcode('[metaslider id=4110]'); ?>
 			</div>
 		</div>
